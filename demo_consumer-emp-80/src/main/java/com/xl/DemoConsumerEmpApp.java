e4ebd2a7d235fabd7979bf8363c5f64457d09724
xiaoluo <473820041@qq.com>
2019-05-31 11:49:09 +0800

    合并前dev

--- demo_consumer-emp-80/src/main/java/com/xl/DemoConsumerEmpApp.java
+++ demo_consumer-emp-80/src/main/java/com/xl/DemoConsumerEmpApp.java
@@ -12,7 +12,8 @@ public class DemoConsumerEmpApp {
 
 	//12321312 
 	public static void main(String[] args) {
-		System.out.println("1235675334111");
+		System.out.println("11");
+		System.out.println("123123");
 		SpringApplication.run(DemoConsumerEmpApp.class, args);
 		
 	}
